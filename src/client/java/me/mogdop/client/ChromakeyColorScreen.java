528bb741db6f52eba29678590da9239256126346
Glob876 <gleb87687@gmail.com>
2026-07-16 17:35:58 +0300

    Refactor ChromakeyColorScreen to use updated owo-lib 0.13.0 UI components naming

--- src/client/java/me/mogdop/client/ChromakeyColorScreen.java
+++ src/client/java/me/mogdop/client/ChromakeyColorScreen.java
@@ -2,9 +2,9 @@ package me.mogdop.client;
 
 import io.wispforest.owo.ui.base.BaseOwoScreen;
 import io.wispforest.owo.ui.component.ColorPickerComponent;
-import io.wispforest.owo.ui.component.Components;
+import io.wispforest.owo.ui.component.UIComponents;
 import io.wispforest.owo.ui.component.TextBoxComponent;
-import io.wispforest.owo.ui.container.Containers;
+import io.wispforest.owo.ui.container.UIContainers;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.*;
 import me.mogdop.ApplyColorPayload;
@@ -18,7 +18,7 @@ public class ChromakeyColorScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
-        return OwoUIAdapter.create(this, Containers::verticalFlow);
+        return OwoUIAdapter.create(this, UIContainers::verticalFlow);
     }
 
     @Override
@@ -29,13 +29,13 @@ public class ChromakeyColorScreen extends BaseOwoScreen<FlowLayout> {
             .verticalAlignment(VerticalAlignment.CENTER);
 
         // Основной вертикальный контейнер для элементов интерфейса
-        FlowLayout container = Containers.verticalFlow(Sizing.content(), Sizing.content());
+        FlowLayout container = UIContainers.verticalFlow(Sizing.content(), Sizing.content());
         container.surface(Surface.DARK_PANEL)
             .padding(Insets.of(12))
             .horizontalAlignment(HorizontalAlignment.CENTER);
 
         // Заголовок окна
-        container.child(Components.label(Text.literal("Select Custom HEX Color")).margins(Insets.vertical(5)));
+        container.child(UIComponents.label(Text.literal("Select Custom HEX Color")).margins(Insets.vertical(5)));
 
         // Компонент встроенной HSV палитры
         ColorPickerComponent colorPicker = new ColorPickerComponent();
@@ -43,7 +43,7 @@ public class ChromakeyColorScreen extends BaseOwoScreen<FlowLayout> {
         colorPicker.margins(Insets.vertical(8));
 
         // Поле ввода HEX-кода
-        TextBoxComponent hexInput = Components.textBox(Sizing.fixed(100));
+        TextBoxComponent hexInput = UIComponents.textBox(Sizing.fixed(100));
         hexInput.setText("#00FF33");
         hexInput.setMaxLength(7);
 
@@ -69,7 +69,7 @@ public class ChromakeyColorScreen extends BaseOwoScreen<FlowLayout> {
         container.child(hexInput.margins(Insets.vertical(6)));
         
         // Кнопка подтверждения
-        container.child(Components.button(Text.literal("Apply"), button -> {
+        container.child(UIComponents.button(Text.literal("Apply"), button -> {
             applyColor(selectedColor);
             this.close();
         }).margins(Insets.vertical(5)));
